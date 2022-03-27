bcfa5a574cd1e00ae2c32c8298a25b3e57d424ba
Oleksandr Omelchenko <olexander63@gmail.com>
2022-03-27 18:41:24 +0300

    Else

---
+++ if/src/If/Main.java
@@ -0,0 +1,13 @@
+package If;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (5 > 1) System.out.println("Пять больше чем один.");
+
+        if (2 < 4) {
+            System.out.println("Два меньше четырех.");
+            System.out.println("Проверка выполнена успещно.");
+        }
+    }
+}
