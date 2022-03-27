1cd5b1f2fa1b3565454135b78ebecf522f468d32
Oleksandr Omelchenko <olexander63@gmail.com>
2022-03-27 20:15:26 +0300

    Switch

---
+++ Switch/src/Switch/Main.java
@@ -0,0 +1,23 @@
+package Switch;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int month = 2, year = 2022, num = 31;
+
+        switch (month) {
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                num = 30;
+                break;
+            case 2:
+                num = (year % 4 == 0) ? 29 : 28;
+                break;
+            default:
+
+            System.out.println(month + "/" + year + ": " + num + "дней");
+        }
+    }
+}
