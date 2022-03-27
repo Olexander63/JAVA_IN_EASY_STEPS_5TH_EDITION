79dbcdf4a48a5bda1c5fbb0fc78916b847fb5084
Oleksandr Omelchenko <olexander63@gmail.com>
2022-03-27 16:03:05 +0300

    Condition update

--- Condition/src/Condition/Main.java
+++ Condition/src/Condition/Main.java
@@ -3,18 +3,18 @@ package Condition;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-        int num1=1357;
+        int num1 = 1357;
         int num2 = 2468;
         String result;
 
-        result = (num1 % 2 != 0) ?"Нечетное":"Четное";
+        result = (num1 % 2 != 0) ? "Нечетное" : "Четное";
 
         System.out.println(num1 + " - " + result);
 
-        result = (num2 % 2 !=0 )? "Нечетное " : "Четное";
-        System.out.println(num2+ " - " + result);
+        result = (num2 % 2 != 0) ? "Нечетное " : "Четное";
+        System.out.println(num2 + " - " + result);
     }
 }
 
