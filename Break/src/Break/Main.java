70852b2e988f82aabb04b2e62a3f4f9de19fdba8
Oleksandr Omelchenko <olexander63@gmail.com>
2022-04-01 12:56:02 +0300

    Break&Continue

--- Break/src/Break/Main.java
+++ Break/src/Break/Main.java
@@ -4,15 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-    for (int i =1; i< 4; i ++){
-        for ( int j=1; j <4; j++){
-
-            if (i == 2){
-                System.out.println("Выход из внутрешнего цикла при i=" +i+" j =" +j);
-                break;
+        for (int i = 1; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                if (i == 1 & j == 1) {
+                    System.out.println("Продолжение работі внутреннего цикда при i=" + i + " j=" + j);
+                    continue;
+                }
+                if (i == 2) {
+                    System.out.println("Выход из внутрешнего цикла при i=" + i + " j =" + j);
+                    break;
+                }
+                System.out.println("Итерация i=" + i + " j =" + j);
             }
-            System.out.println("Итерация i="+i+" j ="+j);
         }
-    }
     }
 }
