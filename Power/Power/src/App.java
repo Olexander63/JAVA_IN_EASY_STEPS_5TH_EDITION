a154d6c282c1c406e94238661352cda6f64a2954
Oleksandr Omelchenko <olexander63@gmail.com>
2022-04-10 23:49:09 +0300

    Power

---
+++ Power/Power/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+        int num = Integer.parseInt(args[0]);
+        int square = (int) Math.pow(num, 2);
+        int cube = (int) Math.pow(num, 3);
+        int sqrt = (int) Math.sqrt(num);
+
+        System.out.println(num + " в квадрате равно" + square);
+        System.out.println(num + " в кубе равно" + cube);
+        System.out.println("Квадратній корень из" + num + " равен" + sqrt);
+    }
+}
