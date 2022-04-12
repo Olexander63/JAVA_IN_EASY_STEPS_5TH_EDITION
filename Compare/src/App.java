9805bc08d87cf91c4f4ee66cc53eccbb9d584385
Oleksandr Omelchenko <olexander63@gmail.com>
2022-04-12 07:32:27 +0300

    Compare

---
+++ Compare/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        float num1 = 24.f;
+        int num2 = 25;
+
+        System.out.println("Наибольшее " + Math.max(num1, num2));
+        System.out.println("Наименьшее " + Math.min(num1, num2));
+
+    }
+}
